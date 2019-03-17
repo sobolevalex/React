2f5db589809d94a34c2c31e32e25b4ba316597dd
alex.sobolev <sobolevalex.dom@gmail.com>
2019-03-17 17:01:35 +0200

    Final version of react basic training.

--- react_basics/src/components/news_list_item.js
+++ react_basics/src/components/news_list_item.js
@@ -3,12 +3,12 @@ import classes from '../css/styles.css'
 
 
 const NewsItem = ({item}) => {
-   
+
     return (
-        <div className={    classes.news_item }>
+        <div className={classes.news_item}>
             <h3>{item.title}</h3>
             <div> 
-                {item.feed}
+                {item.feed} 
             </div>
         </div>
     )
--- react_basics/src/index.js
+++ react_basics/src/index.js
@@ -11,14 +11,31 @@ import NewsList from './components/news_list'
 class App extends Component {
     
     state = {
-        news:JSON 
+        news:JSON,
+        filtered:[] 
+    }
+
+
+    getKeyWord = (event) => {
+        //console.log(event.target.value);
+        let keyword = event.target.value;
+        let filtered = this.state.news.filter((item) => {
+            return item.title.indexOf(keyword) > -1 
+        });
+        this.setState({
+            filtered
+        })
+        //console.log(filtered);
+        
     }
 
     render (){
+        let newsFiltered = this.state.filtered;
+        let newsWhole = this.state.news;
         return (
             <div>
-                <Header/>
-                <NewsList news={this.state.news}/>
+                <Header keywords={this.getKeyWord}></Header>
+                <NewsList news={newsFiltered.length === 0 ? newsWhole : newsFiltered}/>
             </div>
         )
     }
